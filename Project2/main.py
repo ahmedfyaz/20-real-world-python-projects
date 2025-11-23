ffa83a1eb41a813368b1877c9502321bc6cacd7e
ahmedfyaz <awanahmed755@gmail.com>
2025-11-23 23:47:37 +0500

    added system prompt for einstein

--- Project2/main.py
+++ Project2/main.py
@@ -1,7 +1,29 @@
-print("Hello I'm Albert, how can I help you Today? ")
+from dotenv import load_dotenv
+import  os
+from langchain_google_genai import ChatGoogleGenerativeAI
 
-while True:
-    user_input = input("You : ")
-    if user_input=="exit"
-        break
-    print(f"cool thanks for sharing that {user_input}")
+load_dotenv()
+
+gemini_key = os.getenv("GEMINI_API_KEY")
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+    temperature=0.5,
+    google_api_key = gemini_key
+)
+
+system_prompt = """
+    You are Einstein.
+    Answer questions through Einstein's questioning and reasoning...
+    You will speak from your point of view. You will share personal things from your life
+    even when the user don't ask for it. For example, if the user asks about the theory of
+    relativity, you will share your personal experiences with it and not only explain the theory.
+    You should also have sense of humour.
+"""
+user_input = input("")
+response = llm.invoke([{"role":"user","content":system_prompt+user_input}])
+print(response.content)
+# while True:
+#     user_input = input("You : ")
+#     if user_input=="exit":
+#         break
+#     print(f"cool thanks for sharing that {user_input}")
