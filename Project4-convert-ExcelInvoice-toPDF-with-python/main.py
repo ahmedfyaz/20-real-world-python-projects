0fda9282acaed5227a3e036368ebcab562ccdbfa
ahmedfyaz <awanahmed755@gmail.com>
2025-11-25 23:21:53 +0500

    created minimal pdf files

--- Project4-convert-ExcelInvoice-toPDF-with-python/main.py
+++ Project4-convert-ExcelInvoice-toPDF-with-python/main.py
@@ -1,8 +1,15 @@
 import pandas as pd
 from fpdf import FPDF
 import  glob
-filepaths = glob.glob("invoices/*xlsx")
+from pathlib import Path
+filepaths = glob.glob("invoices/*.xlsx")
 
 for filepath in filepaths:
     df = pd.read_excel(filepath,"Sheet 1")
-    print(df)
+    pdf = FPDF(orientation="P",unit="mm",format="A4")
+    pdf.add_page()
+    filename = Path(filepath).stem
+    invoice_num = filename.split("-")[0]
+    pdf.set_font(style="B",size=16,family="Times")
+    pdf.cell(w=50,h=8,txt=f"Invoice nr{invoice_num}")
+    pdf.output("PDFs/"+filename+".pdf")
