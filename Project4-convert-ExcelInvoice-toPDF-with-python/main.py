c044a2d6992fe5cf8b5255d1d3e4f6d8c3b7f633
ahmedfyaz <awanahmed755@gmail.com>
2025-11-26 00:26:57 +0500

    added dates

--- Project4-convert-ExcelInvoice-toPDF-with-python/main.py
+++ Project4-convert-ExcelInvoice-toPDF-with-python/main.py
@@ -5,11 +5,23 @@ from pathlib import Path
 filepaths = glob.glob("invoices/*.xlsx")
 
 for filepath in filepaths:
+    # Collected data from dataframe
     df = pd.read_excel(filepath,"Sheet 1")
+    #set up pdf orientation
     pdf = FPDF(orientation="P",unit="mm",format="A4")
+    # added page
     pdf.add_page()
+
+    # added filename
     filename = Path(filepath).stem
-    invoice_num = filename.split("-")[0]
+    invoice_num,date = filename.split("-")
+
+    #Number
     pdf.set_font(style="B",size=16,family="Times")
     pdf.cell(w=50,h=8,txt=f"Invoice nr{invoice_num}")
+
+    #date
+    pdf.set_font(style="B", size=16, family="Times")
+    pdf.cell(w=50, h=8, txt=f"Invoice nr{invoice_num}")
+
     pdf.output("PDFs/"+filename+".pdf")
