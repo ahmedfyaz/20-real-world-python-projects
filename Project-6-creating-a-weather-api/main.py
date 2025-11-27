5bf1786dd5cd7c54b701b8a983381b3857ab2021
ahmedfyaz <awanahmed755@gmail.com>
2025-11-27 22:32:41 +0500

    initialized home page for the project

--- Project-6-creating-a-weather-api/main.py
+++ Project-6-creating-a-weather-api/main.py
@@ -2,10 +2,7 @@ from flask import Flask,render_template
 
 app =Flask('Website')
 
-@app.route("/home")
+@app.route("/")
 def home():
-    return render_template("tutorial.html")
-@app.route("/about/")
-def about():
-    return render_template("about.html")
+    return render_template("home.html")
 app.run(debug=True)
