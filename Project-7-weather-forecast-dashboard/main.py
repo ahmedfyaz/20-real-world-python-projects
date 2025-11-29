f5f998dd65222dc976079020143f10f0ebd3ee9d
ahmedfyaz <awanahmed755@gmail.com>
2025-11-29 22:26:56 +0500

    imported plotly

--- Project-7-weather-forecast-dashboard/main.py
+++ Project-7-weather-forecast-dashboard/main.py
@@ -1,4 +1,5 @@
 import  streamlit as st
+import plotly.express as px
 st.title("Weather Forecast for the Next Days")
 place = st.text_input("Place:",placeholder="London")
 days = st.slider("Forecast Days:",max_value=5,min_value=1,
@@ -6,3 +7,5 @@ days = st.slider("Forecast Days:",max_value=5,min_value=1,
 option = st.selectbox("Select data to view:",
                       ("Temperature","Sky"))
 st.subheader(f"{option} for the next {days} in {place}")
+figure = px.line()
+st.plotly_chart(days,option.value,)
