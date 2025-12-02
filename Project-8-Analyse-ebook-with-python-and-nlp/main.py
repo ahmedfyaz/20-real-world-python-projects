a540765f623c89cee25d976715e3f29384d0cf4b
ahmedfyaz <awanahmed755@gmail.com>
2025-12-02 22:13:32 +0500

    find the number of the chapter

--- Project-8-Analyse-ebook-with-python-and-nlp/main.py
+++ Project-8-Analyse-ebook-with-python-and-nlp/main.py
@@ -1,2 +1,9 @@
 with open("miracle_in_the_andes.txt") as file:
     book = file.read()
+
+## Finding the number of the chapter
+
+import  re
+pattern = re.compile("Chapter [0-9]+")
+chapters = re.split(pattern,book)
+chapters = chapters[1:]
