9cb291d95bc0478d5672fbb750b431d5db12005b
ahmedfyaz <awanahmed755@gmail.com>
2025-12-03 21:43:32 +0500

    video part finished

--- Project-9-webcam-motion-alert-app/main.py
+++ Project-9-webcam-motion-alert-app/main.py
@@ -1,18 +1,20 @@
-import os
-os.environ["QT_QPA_PLATFORM"] = "xcb"
+import os #you dont have to put these
+os.environ["QT_QPA_PLATFORM"] = "xcb" #you dont have to put these
 import time
 import  cv2
-video = cv2.VideoCapture(0)
-time.sleep(1)
+
+video = cv2.VideoCapture(0) # 0 of you are using your computer or laptop camera and 1 if external
+time.sleep(1) ## it will negate the black color in the starting and help the camera load
+
 while True:
     check,frame = video.read()
-    cv2.imshow("My video",frame)
+    cv2.imshow("My Video",frame)
 
     key = cv2.waitKey(1)
-    if key == ord('q'):
+
+    if key == ord("q"):
         break
 video.release()
-
 
 
 
