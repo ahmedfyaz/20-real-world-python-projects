4727f38e433f270ca73031a0c43fd37034a97243
ahmedfyaz <awanahmed755@gmail.com>
2025-12-08 10:21:45 +0500

    initial commit

--- Project-11-Hotel-Booking/main.py
+++ Project-11-Hotel-Booking/main.py
@@ -2,23 +2,26 @@ import pandas as pd
 
 df = pd.read_csv('hotels.csv')
 class Hotel:
-    def __init__(self,id):
+    def __init__(self,hotel_id):
         pass
+
     def book(self):
         pass
+
     def avaliable(self):
         pass
 
 
 class ReservationTicket:
-    def __init__(self,name,hotel):
+    def __init__(self,customer_name,hotel_object):
         pass
     def generate(self):
         pass
 
+
 print(df)
-id = input("Enter the id of the hotel")
-hotel = Hotel(id)
+hotel_id = input("Enter the id of the hotel")
+hotel = Hotel(hotel_id)
 if hotel.avaliable():
     hotel.book()
     name = input("Enter your name")
