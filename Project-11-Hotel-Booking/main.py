8a6a93341b67295fecece5bdd40284609b84ab41
ahmedfyaz <awanahmed755@gmail.com>
2025-12-08 10:37:33 +0500

    give a bit life to the methods

--- Project-11-Hotel-Booking/main.py
+++ Project-11-Hotel-Booking/main.py
@@ -3,13 +3,22 @@ import pandas as pd
 df = pd.read_csv('hotels.csv')
 class Hotel:
     def __init__(self,hotel_id):
+        self.hotel_id = hotel_id
         pass
 
     def book(self):
+        """Book the Hotel by changing its availablity to no"""
+        df.loc[df["id"] == self.hotel_id,"available"] = "no"
+        df.to_csv()
         pass
 
-    def avaliable(self):
-        pass
+    def available(self):
+        """Check the availablity of the Hotel """
+        availablity = df.loc[df["id"]==self.hotel_id,"available"].squeeze()
+        if availablity == "yes":
+            return True
+        else:
+            return False
 
 
 class ReservationTicket:
@@ -20,8 +29,8 @@ class ReservationTicket:
 
 
 print(df)
-hotel_id = input("Enter the id of the hotel")
-hotel = Hotel(hotel_id)
+hotel_ID = input("Enter the id of the hotel")
+hotel = Hotel(hotel_ID)
 if hotel.avaliable():
     hotel.book()
     name = input("Enter your name")
