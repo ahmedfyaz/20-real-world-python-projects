24bfbb0f0f31e536c5db4b834bdaa98749416130
ahmedfyaz <awanahmed755@gmail.com>
2025-12-11 20:13:29 +0500

    started working on student managment system

--- Project-13-Student-Managment-System/main.py
+++ Project-13-Student-Managment-System/main.py
@@ -1,33 +1,18 @@
-from PyQt6.QtWidgets import QApplication, QVBoxLayout, QLabel, QWidget, QGridLayout, \
-    QLineEdit, QPushButton
+from PyQt6.QtWidgets import (
+    QApplication, QVBoxLayout, QLabel, QWidget,
+    QGridLayout, QLineEdit, QPushButton, QMainWindow
+)
+from PyQt6.QtGui import  QAction
 import sys
 
-class AgeCalculator(QWidget):
+class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        grid = QGridLayout()
+        self.setWindowTitle("Student Managment System")
 
-        # Create Widgets
-        name_label = QLabel("Name:")
-        name_line_edit = QLineEdit()
 
-        date_birth_label = QLabel("Date of Birth MM/DD/YYYY")
-        date_birth_line_edit = QLineEdit()
-
-        calculate_button = QPushButton("Calculate Age")
-        output_label = QLabel("")
-
-        #add Widgets
-        grid.addWidget(name_label,0,0)
-        grid.addWidget(name_line_edit,0,1)
-        grid.addWidget(date_birth_label,1,0)
-        grid.addWidget(date_birth_line_edit,1,1)
-        grid.addWidget(calculate_button,2,0,1,2)
-        grid.addWidget(output_label,3,0,1,2)
-
-        self.setLayout(grid)
 
 app = QApplication(sys.argv)
-age_calculator = AgeCalculator()
-age_calculator.show()
+main_window = MainWindow()
+main_window.show()
 sys.exit(app.exec())
